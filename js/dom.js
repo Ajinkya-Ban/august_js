bc5d27da2ab7e48b5e1122166c3cbe913d6317ef
Ajinkya-Ban <ajinkyaban@gmail.com>
2024-04-26 15:46:23 +0530

    dom code push

---
+++ js/dom.js
@@ -0,0 +1,41 @@
+// var element = document.all[4].innerHTML;
+// console.log(element);
+
+// var element = document.head;
+// console.log(element);
+
+// var element = document.title;
+// console.log(element);
+
+// var element = document.body;
+// console.log(element);
+
+// var element = document.links[2];
+// console.log(element);
+
+// var element = document.images;
+// console.log(element);
+
+// var element = document.forms;
+// console.log(element);
+
+// var element = document.doctype;
+// console.log(element);
+
+// var element = document.URL;
+// console.log(element);
+
+// var element = document.baseURI;
+// console.log(element);
+
+// var element = document.domain;
+// console.log(element);
+
+// var ele = document.getElementById('header').innerHTML;
+// console.log(ele);
+
+// var ele = document.getElementById('header').innerText;
+// console.log(ele);
+
+var ele = document.getElementById('header').setAttribute('class','menu');
+console.log(ele);
