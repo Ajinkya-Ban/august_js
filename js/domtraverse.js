6586c25f5e0607514821a3a17e2263adcf18a0c9
Ajinkya-Ban <ajinkyaban@gmail.com>
2024-04-27 09:52:05 +0530

    dom traverse and password hide/show code

---
+++ js/domtraverse.js
@@ -0,0 +1,11 @@
+// var ele = document.getElementById('child-box').parentElement;
+
+// var ele2 = document.body.parentElement;
+// var ele3 = document.getElementById('main').parentNode
+
+// document.getElementById('child-box').parentElement.style.background="red";
+// var ele = document.getElementById('child-box').parentElement;
+// console.log(ele);
+document.getElementById('sub-child').children[0].style.background="yellow";
+var ele = document.getElementById('sub-child').childNodes;
+console.log(ele);
